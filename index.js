071dcd7c0701a02eb4bc29090193f0dd1606f6ef
MtReginaldo <mateus.sarmento18@gmail.com>
2024-09-09 23:32:43 -0300

    finalizar dia um

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// Hello World
-console.log("Hello World!")
+// Hello World 
+console.log("Hello World!");
+
+// console objeto
+// log função (caixa com funções, nesse caso recebe o dado de texto (string) e apresenta)
+
+let mensagem = 2
+// variável
+const aviso = 2
+// variável que não pode ser alterada 
+console.log(mensagem);
+
+
