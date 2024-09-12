d9f09501439641cc2a65e2ad2226c7189c465de4
MtReginaldo <mateus.sarmento18@gmail.com>
2024-09-11 22:54:10 -0300

    Criar menu da aplicacao

--- index.js
+++ index.js
@@ -17,10 +17,29 @@ function start(){
 // package.json -> Lista dependencias
 // package-lock.json -> Mapeamento de componentes das dependencias
 
-function start(){
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
     while(true){ // menu
         
-        let opcao = "sair"
+                      // esperar o usuario digitar   
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
 
         switch(opcao){
             case "cadastrar":
@@ -30,6 +49,7 @@ function start(){
                 console.log("vamos listar")
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return                
         }
     }
