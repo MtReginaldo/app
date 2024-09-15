e3a62add1445a7aab4f2c902b1f408db733a3603
MtReginaldo <mateus.sarmento18@gmail.com>
2024-09-15 01:48:45 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -73,6 +73,22 @@ const listarMetas = async () => {
     console.log('Meta(s) marcadas como concluída(s)')
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked // se a meta tiver realizada ela vai pra lista 
+    }) 
+
+    if(realizadas.length == 0){
+        console.log("Não existem metas realizadas :(")
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]        
+    })
+}
+
 const start = async () => {
     while(true){ // menu
                       // esperar o usuario digitar  
@@ -88,6 +104,10 @@ const start = async () => {
                     name: "Listar metas",
                     value: "listar"
                 },
+                {                                          
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
                 {
                     name: "Sair",
                     value: "sair"
@@ -101,6 +121,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima!")
